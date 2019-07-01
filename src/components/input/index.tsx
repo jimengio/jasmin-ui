ef0226783c162ec88893a4bc45c79998d4f9b43b
rebirthO <benyue.liu@jimeng.io>
2019-07-01 10:15:24 +0800

    optimize input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 
-import Input, { InputProps } from "antd/lib/input";
+import Input, { InputProps, SearchProps, TextAreaProps, PasswordProps, GroupProps } from "antd/lib/input";
 
 export interface JMInputProps extends InputProps {}
 
+export interface JMSearchProps extends SearchProps {}
+
+export interface JMTextAreaProps extends TextAreaProps {}
+
+export interface JMPasswordProps extends PasswordProps {}
+
+export interface JMGroupProps extends GroupProps {}
+
 export default class JMInput extends React.Component<JMInputProps> {
+  static Search = Input.Search;
+
+  static TextArea = Input.TextArea;
+
+  static Password = Input.Password;
+
+  static Group = Input.Group;
+
   render() {
     const { children, ...others } = this.props;
     return <Input {...others} />;
